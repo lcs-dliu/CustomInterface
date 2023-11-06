328544ef457a5d057fc20f0c4c62e7bf2de95669
Dawn Liu <dliu@lcs.on.ca>
2023-11-06 14:32:03 -0500

    Initial Commit

---
+++ personal work/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  personal work
+//
+//  Created by Chenxi Liu on 2023/11/6.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ personal work/personal_workApp.swift
@@ -0,0 +1,17 @@
+//
+//  personal_workApp.swift
+//  personal work
+//
+//  Created by Chenxi Liu on 2023/11/6.
+//
+
+import SwiftUI
+
+@main
+struct personal_workApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
